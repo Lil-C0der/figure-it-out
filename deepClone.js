906cac3a1ed801aee6d190a95654b1d341ab0f8c
Lil-C0der <781172643@qq.com>
2020-06-24 22:12:03 +0800

    style: 修改 deepClone 函数中的一些变量名

--- deepClone.js
+++ deepClone.js
@@ -2,35 +2,49 @@ function deepClone(source, hash = new WeakMap()) {
   // 查表，如果表中有则返回
   if (hash.has(source)) return hash.get(source);
   // 判断复制的目标是数组还是对象
-  const targetObj = source.constructor === Array ? [] : {};
-  hash.set(source, targetObj);
+  const target = Array.isArray(source) ? [] : {};
+  hash.set(source, target);
+
   // 遍历对象 拷贝每一个值
-  for (let keys in source) {
-    if (source[keys] && typeof source[keys] === "object") {
-      targetObj[keys] = source[keys].constructor === Array ? [] : {};
-      // 如果值是对象则递归
-      targetObj[keys] = deepClone(source[keys], hash);
+  for (let key in source) {
+    if (typeof source[key] === "object" && source[key] !== null) {
+      target[key] = Array.isArray(source[key]) ? [] : {};
+      // 如果值是对象则递归调用
+      target[key] = deepClone(source[key], hash);
     } else {
       // 直接赋值
-      targetObj[keys] = source[keys];
+      target[key] = source[key];
     }
   }
-  return targetObj;
+
+  return target;
 }
 
 let obj = {
   b: "aaa",
   c: null,
+  d: {
+    d1: "d1",
+    d2: "d2",
+  },
 };
 obj.a = obj;
 
 let newObj = deepClone(obj);
-newObj.b = "ccc";
-console.log(newObj);
-console.log(obj);
+
+newObj.b = "bbb";
+newObj.c = "c";
+newObj.d.d1 = "D1";
+
+obj.b = "bb";
+
+console.log(obj); // { b: 'bb', c: null, d: { d1: 'd1', d2: 'd2' }, a: [Circular] }
+console.log(newObj); // { b: 'bbb', c: 'c', d: { d1: 'D1', d2: 'd2' }, a: [Circular] }
 
 let arr = [1, 2, 3, 4, 5, [2, 3]];
 let newArr = deepClone(arr);
 newArr[0] = 0;
+arr[1] = { a: 1, b: 2 };
+
 console.log(newArr);
 console.log(arr);
