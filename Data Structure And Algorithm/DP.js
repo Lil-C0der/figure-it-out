be660e5f1dd140d6b2004bdd44ab4fd1cde99575
Lil-C0der <howtousegmai1@gmail.com>
2020-11-22 16:20:29 +0800

    feat: fib

--- Data Structure And Algorithm/DP.js
+++ Data Structure And Algorithm/DP.js
@@ -1,4 +1,20 @@
 /**
+ * 剑指 Offer 10- I. 斐波那契数列
+ * @param {number} n
+ * @return {number}
+ */
+function fib(n) {
+  let dp = [];
+  dp[0] = 0;
+  dp[1] = 1;
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
+}
+// console.log(fib(6));
+
+/**
  * 剑指 Offer 14- I. 剪绳子
  * 一根长度为 n （n >= 2）的绳子，把绳子剪成整数长度的 m 段（m > 1）
  * 每段绳子的长度记为 k[0],k[1]...k[m-1] 。请问 k[0]*k[1]*...*k[m-1] 可能的最大乘积是多少
