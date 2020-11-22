0aef8dae14eda55eeacf86016d9ecb5ca7c83bec
Lil-C0der <howtousegmai1@gmail.com>
2020-11-22 15:51:16 +0800

    feat: cuttingRope

--- Data Structure And Algorithm/DP.js
+++ Data Structure And Algorithm/DP.js
@@ -7,15 +7,42 @@
  * @return {number}
  */
 function cuttingRope(n) {
+  // dp[n] 表示长度为 n 的绳子切割后的最大乘积
   let dp = [];
   dp[2] = 1;
-  for (let i = 0; i <= n; i++) {
-    dp[i] = 1 * (i - 1);
-    for (let j = 0; j <= i / 2; j++) {
+  for (let i = 3; i <= n; i++) {
+    // 当 n >= 3 时，2 * (n - 2) >= 1 * (n - 1)，所以可以先将绳子分为 2 和 n - 2 两段
+    dp[i] = 2 * (i - 2);
+    // 对 n - 2 这段绳子继续划分，同理从 2 开始
+    for (let j = 2; j <= i / 2; j++) {
+      // 对于有些长度的绳子（例如2、3）继续划分取 dp[2]、dp[3] 反而会使乘积更小，直接相乘则可以提供更大的长度
       dp[i] = Math.max(j * dp[i - j], j * (i - j), dp[i]);
     }
   }
 
   return dp[n];
 }
-console.log(cuttingRope(10)); // 2 * 3 * 3 = 18
+// console.log(cuttingRope(10)); // 2 * 3 * 3 = 18
+
+// 另一种解法：dp[n] 表示的意义不同
+function cuttingRope2(n) {
+  // 当 n <= 3 时，绳子在切割后被分为 1 和 n - 1 两段，此时能够贡献的乘积反而更小
+  // 所以不适用 dp[n]
+  if (n <= 3) {
+    return n - 1;
+  }
+  // dp[n] 表示长度为 n 的绳子能够提供（贡献）的最大乘积
+  let dp = [];
+  dp[1] = 1;
+  dp[2] = 2;
+  dp[3] = 3;
+  for (let i = 4; i <= n; i++) {
+    dp[i] = Number.MIN_SAFE_INTEGER;
+    for (let j = 2; j < i; j++) {
+      dp[i] = Math.max(dp[i], j * dp[i - j]);
+    }
+  }
+
+  return dp[n];
+}
+// console.log(cuttingRope2(10));
