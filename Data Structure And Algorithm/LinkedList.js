eb44a7967cd812f4ac3fa1fe79b420f1ef397046
Lil-C0der <howtousegmai1@gmail.com>
2020-11-23 20:00:35 +0800

    feat: deleteNode 删除链表的节点

--- Data Structure And Algorithm/LinkedList.js
+++ Data Structure And Algorithm/LinkedList.js
@@ -36,3 +36,35 @@ var reverseList = function (head) {
   // 当 curr 为 NULL 时，表示翻转完成，返回前一个节点
   return prev;
 };
+
+/**
+ * 剑指 Offer 18. 删除链表的节点
+ * @param {ListNode | null} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+function deleteNode(head, val) {
+  if (!head) {
+    return null;
+  }
+  let curr = head;
+  // 保存上一个节点
+  let prev = null;
+  while (curr) {
+    if (curr.val === val) {
+      // prev 为空时，curr 指向头节点，表示需要被删除的是头节点，所以直接返回头节点的下一个节点
+      if (!prev) {
+        return curr.next;
+      } else {
+        // 当不为头节点的时候，改变指针指向
+        prev.next = curr.next;
+      }
+    }
+    prev = curr;
+    curr = curr.next;
+  }
+  return head;
+}
+// 分别删除中间的节点和头节点
+// console.log(deleteNode(linkList, 3));
+// console.log(deleteNode(linkList, 1));
