8c5f722f23f9d40bc34bde4a0f3f7adec74fbfa9
Lil-C0der <howtousegmai1@gmail.com>
2020-11-26 22:49:28 +0800

    feat: 简单的前后端分离 demo，通过 CORS 完成跨域请求

---
+++ ajax/server/app.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const urlParser = require('url');
+const queryString = require('querystring');
+
+const server = http.createServer();
+
+server.on('request', (request, response) => {
+  const { url, method, headers } = request;
+  // 获取 url 中携带的数据
+  const params = urlParser.parse(url);
+  const queryObj = queryString.parse(params.query);
+  const responseHeaders = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  };
+
+  if (method === 'POST') {
+    let body = [];
+    request
+      .on('data', (chunk) => {
+        body.push(chunk);
+      })
+      .on('end', () => {
+        body = Buffer.concat(body).toString();
+        const responseBody = { method, url, headers, body };
+        response.writeHead(200, responseHeaders);
+        response.write(JSON.stringify(responseBody));
+        response.end();
+      });
+  }
+  if (method === 'GET') {
+    const data = queryObj;
+    data.key = queryObj.key + queryObj.username;
+    data.greeting = `hello ${data.username}, your key is ${data.key}`;
+
+    const responseBody = { url, method, headers, data };
+    response.writeHead(200, responseHeaders);
+    response.write(JSON.stringify(responseBody));
+    response.end();
+  }
+});
+
+server.listen('8080').on('listening', () => {
+  console.log('server is running at port 8080');
+});
