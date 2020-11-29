5127b77d45b4c9552039da247be1a5df3a95f9fc
Lil-C0der <howtousegmai1@gmail.com>
2020-11-29 17:40:38 +0800

    feat: DP，最长公共子序列

--- Data Structure And Algorithm/DP.js
+++ Data Structure And Algorithm/DP.js
@@ -170,3 +170,48 @@ function uniquePaths(m, n) {
   }
   return dp[m - 1][n - 1];
 }
+
+/**
+ * leetcode 1143. 最长公共子序列
+ * 给定两个字符串 text1 和 text2，返回这两个字符串的最长公共子序列的长度。
+ * 一个字符串的子序列：由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+ * 例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。两个字符串的「公共子序列」是这两个字符串所共同拥有的子序列。
+ * 若这两个字符串没有公共子序列，则返回 0。
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ * 两个字符串的 dp，需要建立二维数组
+ * dp[i][j]：text1 长度为 i + 1 的子串，和 text2 长度为 j + 1 的子串，公共子序列的长度
+ */
+function longestCommonSubsequence(text1, text2) {
+  const row = text1.length,
+    col = text2.length;
+
+  let dp = new Array(row);
+
+  for (let i = 0; i < row; i++) {
+    dp[i] = new Array(col).fill(0);
+    for (let j = 0; j < col; j++) {
+      if (text1[i] === text2[j]) {
+        // 当有匹配的字符，且其中一个子串的长度为 1 时，公共子序列的长度只能为 1
+        if (i === 0 || j === 0) {
+          dp[i][j] = 1;
+        } else {
+          dp[i][j] = dp[i - 1][j - 1] + 1;
+        }
+      } else {
+        // 没有匹配的字符时
+        if (i === 0 && j === 0) {
+          dp[i][j] = 0;
+        } else if (i === 0) {
+          dp[i][j] = dp[i][j - 1];
+        } else if (j === 0) {
+          dp[i][j] = dp[i - 1][j];
+        } else {
+          dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+        }
+      }
+    }
+  }
+  return dp[row - 1][col - 1];
+}
